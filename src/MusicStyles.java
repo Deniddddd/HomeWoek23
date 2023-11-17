e47d985c59a0fdc078aca0763b3f4e8a6da27c38
Deniddddd <dimadima20dima@gmail.com>
2023-11-17 08:44:31 +0200

    first commit

---
+++ src/MusicStyles.java
@@ -0,0 +1,4 @@
+public class MusicStyles {
+    public void playMusic() {
+    }
+}
---
+++ src/PopMusic.java
@@ -0,0 +1,8 @@
+class PopMusic extends MusicStyles {
+
+@Override
+
+public void playMusic() {
+        System.out.println("pop music");
+        }
+}
